41f36f03795e9f5a69ab8a086511dfb4f46bfb39
ThatIsANoNoWord <dharmawan.jo@gmail.com>
2026-04-29 00:03:26 -0500

    Skip function for Q1

--- src/Pages/QuestionOne.jsx
+++ src/Pages/QuestionOne.jsx
@@ -4,6 +4,7 @@ import { Button, Col, Container, Image, Row } from 'react-bootstrap'
 function QuestionOne(prop) {
 
     const [buttonStates, setButtons] = useState();
+    const [pressQuant, setPressQuant] = useState(0);
 
     useEffect(() => {
         let array = [];
@@ -36,7 +37,10 @@ function QuestionOne(prop) {
                     return <Row key={"Row " + index1} xs={5}>
                         {ar.map((state, index2) => {
                             return <Col key={"Col " + index2}>
-                                <Button variant={buttonStates[index1][index2] ? "primary" : "danger"} onClick={() => setButtons(buttonStates.map((arr, i1) => arr.map((item, i2) => Math.abs(i1 - index1) + Math.abs(i2 - index2) <= 1 ? !item : item)))}>
+                                <Button variant={buttonStates[index1][index2] ? "primary" : "danger"} onClick={() => {
+                                    setButtons(buttonStates.map((arr, i1) => arr.map((item, i2) => Math.abs(i1 - index1) + Math.abs(i2 - index2) <= 1 ? !item : item)));
+                                    setPressQuant(e => e + 1);
+                                    }}>
                                     {buttonStates[index1][index2] ? " On! " : " Off! "}
                                 </Button>
                             </Col>
@@ -44,6 +48,19 @@ function QuestionOne(prop) {
                     </Row>
                 })}
             </Container>
+            <br/>
+            {
+                pressQuant > 20
+                ?
+                <Button variant="primary" onClick={() => {
+                    setButtons(buttonStates.map((arr, i1) => arr.map((item, i2) => true)));
+                    setPressQuant(e => e + 1);
+                    }}>
+                    Skip
+                </Button>
+                :
+                <></>
+            }
         </div>
     </div>
   )
